9e763d8fbcfedd1907c31d4bd0904ee290bb44d6
Rodrigo <vr.rodrigo98@gmail.com>
2024-06-19 18:38:05 +0200

    agregando primeros ejercicios

---
+++ base_c/firts_steps.c
@@ -0,0 +1,11 @@
+/*
+Escriba un programa que muestre en la pantalla un mensaje de saludo, por ejemplo
+"Hola",y luego muestre el mensaje "Por favor introduzca el año en que nació". El
+programa debe leer ese valor y almacenarlo en una variable de tipo entero llamada
+fecha. Por último haga que el programa escriba la frase " Si usted nació en <valor de la
+fecha leída> este año cumple <edad calculada> años.
+Es recomendable que vaya escribiendo el programa por pasos (mostrar saludo, pedir
+fecha, leer fecha,…) y compilándolo después de cada paso para comprobar que
+funciona
+*/
+¡
